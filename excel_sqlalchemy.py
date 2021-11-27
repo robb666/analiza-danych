4733b729e6d9c68edb21a19d301f55994f243e6c
Robb <56496611+robb666@users.noreply.github.com>
2021-11-27 19:23:29 +0100

    update

--- excel_sqlalchemy.py
+++ excel_sqlalchemy.py
@@ -6,21 +6,21 @@ import pandas as pd
 input = '/home/robb/Desktop/2014 BAZA MAGRO.xlsx'
 output = 'output.xlsx'
 
-engine = create_engine('sqlite://', echo=False)
+engine = create_engine('sqlite:///baza_sql.db', echo=False)
 
 
 pd.set_option('display.max_rows', 40)
 pd.set_option('display.width', None)
 pd.set_option('display.max_columns', None)
 
-df = pd.read_excel(input, sheet_name='BAZA 2014', header=1)
-new_header = df.iloc[0]
-df = df[1:]
-df.columns = new_header
-print(df)
+# df = pd.read_excel(input, sheet_name='BAZA 2014', header=1)
+# new_header = df.iloc[0]
+# df = df[1:]
+# df.columns = new_header
+# print(df)
 
-df.to_sql('baza', engine)#, if_exists='replace', index_label=False)
-results = engine.execute('Select * from baza where Imie="Robert" and Przypis >= 1000')
+# df.to_sql('baza', engine)#, if_exists='replace', index_label=False)
+results = engine.execute('Select * from baza where Przypis=500')
 final = pd.DataFrame(results)#, columns=df.columns[:152])
 final.to_excel(output, index=False)
 
