7fe7dde7c80ddd1ed6ce96ce4b13b790b2a4a45b
Robb <56496611+robb666@users.noreply.github.com>
2021-11-28 10:29:33 +0100

    Update excel_sqlalchemy.py

--- excel_sqlalchemy.py
+++ excel_sqlalchemy.py
@@ -21,45 +21,49 @@ pd.set_option('display.max_rows', None)
 pd.set_option('display.width', None)
 pd.set_option('display.max_columns', None)
 
-df = pd.read_excel(input, sheet_name='BAZA 2014', header=1)
-new_header = df.iloc[0]
-df = df[1:]
-df.columns = new_header
-print(df.head())
-df.to_sql('baza', engine,
-          dtype={'id': Column('id', Integer, primary_key=True),
-                 'Data wystawienia': DateTime(),
-                 'Przypis': Integer()})  # , if_exists='replace', index_label=False)
-
-results = engine.execute(
-    """
-    Select * from baza where (Imie="Adam" or Imie="ADAM")
-    and Przypis <= 100
-    and "Data wystawienia">="2020-07-20 00:00:00.000000"
-    """)
-
-
-# class User(Base):
-#     __tablename__ = 'user'
+# df = pd.read_excel(input, sheet_name='BAZA 2014', header=1)
+# new_header = df.iloc[0]
+# df = df[2:]
+# df.columns = new_header
 #
-#     key = Column('id', Integer, primary_key=True)
-#     data = Column('Data wystawienia', DateTime())
-#     przypis = Column('Przypis', Integer())
-#     # stardust = Column('stardust', Integer, unique=True)
+# df.to_sql('baza', engine,
+#           dtype={'Data wystawienia': DateTime(),
+#                  'Przypis': Integer()},
+#           if_exists='replace')
 #
-#     def __init__(self, data, przypis):
-#         self.username = data
-#         self.password = przypis
+# results = engine.execute("""
+#                          Select * from baza
+#                          """)
 #
+# final = pd.DataFrame(results)  #, columns=df.columns[:152])
+# ## final.to_excel(output, index=False)
 #
-# Base.metadata.create_all(bind=engine)
-# session = sessionmaker(bind=engine)()
-# # results = session.query(engine).all()
-# users = session.query(User).all()
-# for user in users:
-#     print(user)
+# print(final.head())
 
-final = pd.DataFrame(results)  #, columns=df.columns[:152])
-# final.to_excel(output, index=False)
 
-print(final)
+
+
+class User(Base):
+    __tablename__ = 'baza'
+
+    key = Column('index', Integer, primary_key=True)
+    data = Column('Data wystawienia', DateTime())
+    przypis = Column('Przypis', Integer())
+
+    def __init__(self, key, data, przypis):
+        self.key = key
+        self.username = data
+        self.password = przypis
+
+
+Base.metadata.create_all(bind=engine)
+session = sessionmaker(bind=engine)()
+# results = session.query(engine).all()
+users = session.query(User).filter(User.przypis > 4000).all()
+print(f'Data                Przypis')
+for user in users:
+    print(f'{user.data} {user.przypis}')
+
+
+
+
