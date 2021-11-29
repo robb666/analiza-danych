7f393d4ef7725596e6e3b09260ed7abd02305824
Robb <56496611+robb666@users.noreply.github.com>
2021-11-29 18:42:32 +0100

    Update excel_sqlalchemy.py

--- excel_sqlalchemy.py
+++ excel_sqlalchemy.py
@@ -7,10 +7,12 @@ from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker, relationship
 from sqlalchemy import func
 from sqlalchemy import Date
+import datetime
 import sqlite3
 import pandas as pd
 import matplotlib.pyplot as plt
 import seaborn as sns
+from itertools import cycle
 
 Base = declarative_base()
 
@@ -30,22 +32,39 @@ pd.set_option('display.max_columns', None)
 # df.reset_index()
 #
 # df.to_sql('baza', engine,
-#           dtype={'Data wystawienia': Date,
-#                  'Przypis': Integer},
+#           dtype={
+#                  'Data wystawienia': Date,
+#                  'Przypis': Integer,
+#                  'TU Inkaso': Integer,
+#                  },
 #           if_exists='replace')
 #
-# results = engine.execute("""Select Przypis from baza where Przypis > 9000""")
+# # results = engine.execute("""Select * from baza""")
+# ## final.to_excel(output, index=False)
 #
-# final = pd.DataFrame(results)  #, columns=df.columns[:152])
-# # ## final.to_excel(output, index=False)
-# print(final.head())
+# # print(final)
 
-print()
-df = pd.read_sql('Select "Data wystawienia", Przypis, Firma, Nazwisko from baza where Przypis > 6000', engine)
-df = df.fillna(df['Nazwisko'])
-print(df)
 
-# sns.set(rc={'figure.figsize': (6, 6)});fig, ax = plt.subplots();fig.autofmt_xdate()
-# ax = sns.barplot(x='Data wystawienia', y='Przypis', data=df, hue='Firma')
-# # ax.set_title('Płeć osób skladajcych zapytania.')
-# plt.show()
+
+# print()
+df = pd.read_sql('Select * from baza', engine)
+# # df2 = df['FIRMA'].fillna(df['Nazwisko'])
+
+df = df.rename(columns={'TU Inkaso': 'Inkaso w PLN --> przychód'})
+df_msc = pd.Series(df['Miesiąc przypisu'].replace({'_': ''}, regex=True))#.str.slice(stop=2)
+
+df.insert(3, 'Strzałka czasu', df_msc)
+df = df.sort_values(by=['Strzałka czasu'])
+dff = df.groupby(['Strzałka czasu']).sum()
+rok_msc = df['Strzałka czasu'].unique()
+print(rok_msc)
+
+sns.set(rc={'figure.figsize': (7, 7)});fig, ax = plt.subplots();fig.autofmt_xdate()
+ax = sns.lineplot(x='Strzałka czasu', y='Inkaso w PLN --> przychód', data=dff, ci=68)
+# ax = sns.lineplot(x='Strzałka czasu', y='Przypis', data=dff)
+msc = ['styczeń', 'luty', 'marzec', 'kwiecień', 'maj', 'czerwiec', 'lipiec',
+       'sierpień', 'wrzesień', 'październik', 'listopad', 'grudzień']
+rok = [rok[:2] for rok in rok_msc if rok is not None]
+ax.set_xticklabels(labels=[f'\'{rok} {msc}' for rok, msc in zip(rok, cycle(msc))], rotation=40)
+ax.set_title('INKASO')
+plt.show()
