4f26c75f58795a2c6c7aae78fbd191ff5637456e
Robb <56496611+robb666@users.noreply.github.com>
2021-12-25 14:47:42 +0100

    Update income_vs_costs.py

--- income_vs_costs.py
+++ income_vs_costs.py
@@ -104,20 +104,25 @@ def plot(db, df_bank):
 
     df_bank['Data nowa'] = df_bank['Data księgowania'].apply(lambda x: x[3:])
 
-    # df_bank['Data księgowania'] = df_bank['Data księgowania']#.map(lambda x: pd.to_datetime(x))#.strftime('%d-%m-%Y'))
+
 
     # df_bankv = ax.xaxis.update_units(df_bank['Data księgowania'])
-    new_df = df_bank[['Data nowa', 'Saldo']]
-    df_bankv = new_df.groupby(['Data nowa']).sum()
-    df_bankv = df_bankv.sort_values(by=['Data nowa']).reset_index()
-    print(df_bankv)
+    df_bank = df_bank[['Data nowa', 'Saldo']]
+    df_bank = df_bank.groupby(['Data nowa']).sum().reset_index()
+
+    df_srt = df_bank.sort_values(by=['Data nowa'])
+    df_srt['Data nowa'] = pd.to_datetime(df_srt['Data nowa'])
+    df_bank2 = df_srt.sort_values('Data nowa').reset_index()
+    print(df_bank2)
+
+
 
     # print(df_bank.dtypes)
 
     ax = sns.regplot(x='Data nowa',
     # ax = sns.regplot(x='Data księgowania',
                      y='Saldo',
-                     data=df_bankv,
+                     data=df_bank2,
                      # scatter=None,
                      order=2,
                      scatter_kws={'s': 10, 'alpha': 0.4},
